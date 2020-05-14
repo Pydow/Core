b0550bb0efdc94a392160b7736d9b051c0465c56
Mundschutziii <37897528+Mundschutziii@users.noreply.github.com>
2020-05-14 20:02:55 +0200

    Added join/quit messages

--- src/main/java/net/lldv/pydow/core/listener/PlayerListener.java
+++ src/main/java/net/lldv/pydow/core/listener/PlayerListener.java
@@ -3,12 +3,23 @@ package net.lldv.pydow.core.listener;
 import cn.nukkit.event.EventHandler;
 import cn.nukkit.event.Listener;
 import cn.nukkit.event.player.PlayerJoinEvent;
+import cn.nukkit.event.player.PlayerQuitEvent;
+import cn.nukkit.player.Player;
+import net.lldv.pydow.core.components.language.Language;
 
 public class PlayerListener implements Listener {
 
     @EventHandler
     public void onJoin(PlayerJoinEvent event) {
-        event.getPlayer().setCheckMovement(false);
+        Player player = event.getPlayer();
+        player.setCheckMovement(false);
+        event.setJoinMessage(Language.getAndReplaceNoPrefix("player-joined", player.getName()));
+    }
+
+    @EventHandler
+    public void onQuit(PlayerQuitEvent event) {
+        Player player = event.getPlayer();
+        event.setQuitMessage(Language.getAndReplaceNoPrefix("player-quit", player.getName()));
     }
 
 }
